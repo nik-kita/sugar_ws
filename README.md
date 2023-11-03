c1571e406518f8c7be347e2ba68f0a46ab34be98
nik <nikita.nodejs@gmail.com>
2023-11-03 23:30:54 +0100

    mvp

---
+++ README.md
@@ -0,0 +1,53 @@
+# SugarWs
+```ts
+export class SugarWs extends WebSocket {
+  // implementation
+}
+```
+> So you can see that this websocket is not complicated extension of original browser's (so and __Deno__'s) websocket.
+
+## Important improvements:
+* `.once()` this method has the same API as the original `.addEventListener()` with only difference that it will auto-remove listener after first call
+* `.wait_for(state: 'open' | 'close')` add more simple control over final states of websocket. So you can `await` for open or close and than do your stuff
+  * important to note that this method only waiting for `open` or `close` states but not acting them
+  * `.wait_for('close').and_close()` syntax sugar for:
+    ```ts
+    sugar_ws.close();
+    await sugar_ws.wait_for('close');
+    // the same as
+    await sugar_ws.wait_for('close').and_close();
+    // BUT NOT AS
+    (await sugar_ws.wait_for('close')).close(); // <= should not work because you wait to DO close only when close happen)))
+    ```
+  * in any case be careful with usage of these methods especially in repeated scenarios... more tests are needed for this feature
+* `.send_if_open()` will send only if websocket's `readyState` is `OPEN`
+* `.on()` alias for original's `.addEventListener()`...so this is sugar only
+
+## Example of usage:
+```ts
+import {
+  assert,
+  assertThrows,
+} from "https://deno.land/std@0.205.0/assert/mod.ts";
+import { delay } from "https://deno.land/std@0.205.0/async/delay.ts";
+import { SugarWs } from "./mod.ts";
+
+
+const ws = await new SugarWs(`ws://localhost:3333`).wait_for("open");
+assert(ws.readyState === ws.OPEN, '`.wait_for("open")` is not work');
+ws.onmessage = ({ data }) => console.log(data);
+ws.send_if_open("ping");
+ws.once("message", ({ data }) => onceMessages.push(data));
+await delay(5_000);
+await ws.wait_for("close").and_close();
+assert(ws.readyState === ws.CLOSED, '`.wait_for("close").and_close()` is not working');
+assertThrows(() => ws.send("hi!"), "should not send because ws is closed already");
+assert(new Boolean(ws.send_if_open("hi!")), "ws is close => should not send => no error");
+```
+
+### P.S.
+> Not enough tested the behavior for situations when `.wait_for(...)` promises started without resolving. For example start wait on open but websocket will never do it.
+> In future the additional under-the-hood protections, may be options should be created to handle or auto-handle such situations.
+
+[see code](./mod.ts)
+
---
+++ mod.test.ts
@@ -0,0 +1,153 @@
+import {
+  assert,
+  assertThrows,
+} from "https://deno.land/std@0.205.0/assert/mod.ts";
+import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { delay } from "https://deno.land/std@0.205.0/async/delay.ts";
+import { ApplicationCloseEvent } from "https://deno.land/x/oak@v12.6.1/application.ts";
+import { SugarWs } from "./mod.ts";
+
+const gen_log = (prefix: string, margin_right = 0) => (...args: unknown[]) =>
+  console.debug(`${" ".repeat(margin_right)}${prefix}:`, ...args);
+
+Deno.test("Ws", async () => {
+  const server = gen_test_ws_server();
+
+  await server.started;
+
+  const ws = await new SugarWs(`ws://localhost:${server.port}`).wait_for(
+    "open",
+  );
+  assert(ws.readyState === ws.OPEN, '`.wait_for("open")` is not work');
+  const wsLog = gen_log("ws", 40);
+
+  ws.onerror = (err) => wsLog(err);
+  ws.onclose = () => {
+    wsLog("closed");
+  };
+  ws.onmessage = ({ data }) => wsLog(data);
+  ws.send_if_open("ping");
+
+  const allMessages = [] as MessageEvent["data"][];
+  const onceMessages = [] as MessageEvent["data"][];
+
+  ws.on("message", ({ data }) => {
+    allMessages.push(data);
+    ws.send_if_open("ping");
+  });
+  ws.once("message", ({ data }) => onceMessages.push(data));
+  await delay(1);
+  await ws.wait_for("close").and_close();
+  assert(
+    ws.readyState === ws.CLOSED,
+    '`.wait_for("close").and_close()` is not working',
+  );
+  assertThrows(
+    () => ws.send("hi!"),
+    "should not send because ws is closed already",
+  );
+  assert(
+    new Boolean(ws.send_if_open("hi!")),
+    "ws is close => should not send => no error",
+  );
+  assert(allMessages.length > 1, "this is important condition for this test");
+  assert(onceMessages.length === 1, "`.once()` is not working");
+  server.stop_signal();
+  await server.listening;
+});
+
+type GenWsServerOptions = {
+  port: number;
+  log: (...args: unknown[]) => void;
+  configure_client_ws: (ws: WebSocket) => void;
+};
+
+const default_port = 3333;
+const gen_default_client_ws_manipulator =
+  (log: (...args: unknown[]) => void) => (client_ws: WebSocket) => {
+    let counter = 0;
+    client_ws.onopen = () => {
+      log("server: client_ws is open");
+      client_ws.send("do you want to play tennis?");
+    };
+    client_ws.onerror = log;
+    client_ws.onmessage = ({ data }) => {
+      log("server: ", data);
+      client_ws.send("pong " + ++counter);
+    };
+  };
+
+function gen_test_ws_server(
+  options: Partial<GenWsServerOptions> = {},
+) {
+  const {
+    log = console.debug,
+    port = default_port,
+  } = options;
+  const configure_client_ws = options.configure_client_ws ||
+    gen_default_client_ws_manipulator(log);
+  const abortController = new AbortController();
+  const { signal } = abortController;
+  const clients_close_promises = [] as Promise<void>[];
+  const clients = [] as WebSocket[];
+  const server = new Application()
+    .use((ctx) => {
+      ctx.isUpgradable || ctx.throw(400, "ws connection expected");
+
+      const clientWs = ctx.upgrade();
+      clients.push(clientWs);
+      clients_close_promises.push(
+        new Promise((resolve) =>
+          clientWs.addEventListener("close", () => resolve())
+        ),
+      );
+      configure_client_ws(clientWs);
+    });
+
+  const {
+    promise: graceful_shutdown,
+    resolveContainer,
+  } = (() => {
+    const resolveContainer = [] as ((value: unknown) => void)[];
+    const promise = new Promise((resolve) => {
+      resolveContainer.push(resolve);
+    });
+
+    return { promise, resolveContainer };
+  })();
+
+  server.addEventListener("close", async () => {
+    log("server: closing...");
+    clients.forEach((c) =>
+      [WebSocket.CLOSING, WebSocket.CLOSING].includes(c.readyState) ||
+      c.close(1000)
+    );
+    for await (const closing of clients_close_promises) {
+      await closing;
+    }
+    const last_promise = resolveContainer.pop();
+    if (!last_promise) {
+      console.error(
+        "LOW_LEVEL_TEST_ERROR: unable to resolve last promise for graceful shutdown",
+      );
+    } else last_promise("complete");
+  });
+
+  const started = new Promise((resolve, reject) => {
+    server.addEventListener("error", reject);
+    server.addEventListener("listen", resolve);
+  });
+
+  return {
+    port,
+    started,
+    listening: server.listen({
+      port,
+      signal,
+    }).then(() => graceful_shutdown),
+    stop_signal: abortController.abort.bind(
+      abortController,
+      new ApplicationCloseEvent({}),
+    ),
+  };
+}
